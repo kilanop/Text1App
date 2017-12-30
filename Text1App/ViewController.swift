127d03ff7fa472729a1dd50960147ec46969a008
wein <aaa@gmail.com>
2017-12-30 10:13:38 +0800

    Initial Commit

---
+++ Text1App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text1App
+//
+//  Created by ios on 2017/12/30.
+//  Copyright © 2017年 pcschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
